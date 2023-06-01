8e4f496c587af9024056956ded2578dc497e7f43
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-06-01 12:57:55 +0700

    Add files via upload

---
+++ Inheritance/22.11.4856/Orang.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Orang
+    {
+        // Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Constructor
+        public Orang(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public Orang()
+        {
+
+        }
+
+        // Method
+        public void InfoOrang()
+        {
+            Console.WriteLine("Nama saya {0} dan umur saya {1} tahun.", this.Name, this.Age);
+        }
+    }
+
+    // Anak Kelas
+    public class Karyawan : Orang
+    {
+        // Properties
+        public string KaryawanID { get; set; }
+        public string Subject { get; set; }
+
+        // Constructor
+        public Karyawan(string KaryawanID, string Subject)
+        {
+            this.KaryawanID = KaryawanID;
+            this.Subject = Subject;
+        }
+        public Karyawan()
+        {
+
+        }
+
+        // Override Method
+        public void InfoOrang(string Subject, string KaryawanID)
+        {
+            this.Subject = Subject;
+            this.KaryawanID = KaryawanID;
+            Console.WriteLine("Saya disini menjabat sebagai {0} dengan ID Karyawan {1}.", this.Subject, this.KaryawanID);
+        }
+    }
+}
---
+++ Inheritance/22.11.4856/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orang Orang1 = new Orang();
+            Orang1.Name = "Ecanto Rusdy Hassan";
+            Orang1.Age = 19;
+            Orang1.InfoOrang();
+            Console.WriteLine();
+
+            // Inheritance
+            Karyawan Karyawan1 = new Karyawan();
+            Karyawan1.Name = "Jose Husein";
+            Karyawan1.Age = 34;
+            Karyawan1.InfoOrang();
+            Karyawan1.InfoOrang("Dekan", "DKN119923");
+
+            Console.ReadLine();
+        }
+    }
+}
